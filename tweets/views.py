80e2f4aa22843755f866475f766ed78e952c3646
Ineldo <thununo1@hotmail.com>
2022-11-07 11:34:03 -0100

    Bootstrap & Django Templates, Tweet List View

--- tweets/views.py
+++ tweets/views.py
@@ -4,9 +4,18 @@ from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello Tweet<h1>")
+    # return HttpResponse("<h1>Hello Tweet<h1>")
     return render(request,'pages/home.html',context={}, status=200)
 
+def tweet_list_view(request, *args, **kwargs):
+    qs= Tweet.objects.all()
+    tweet_list=[{"id":x.id, "content":x.content} for x in qs]
+    data= {
+        "response":tweet_list
+    }
+    return JsonResponse(data)
+
+    
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     data={
         "id":tweet_id
